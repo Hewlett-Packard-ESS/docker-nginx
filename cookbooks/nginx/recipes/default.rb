f73be10dc49e4824aff92aac02623257de09f3bc
Karl Stoney <karl@jambr.co.uk>
2015-01-03 16:34:44 +0000

    Further configuration

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -5,7 +5,7 @@ directory '/storage/nginx' do
   recursive true
 end
 
-# This is a fudge as chef doesn't seem to sec permissions all
+# This is a fudge as chef doesn't seem to set permissions all
 # the way up a recursive tree!
 directory "/storage/nginx/conf.d" do
   owner 'nginx'
@@ -14,8 +14,23 @@ directory "/storage/nginx/conf.d" do
   recursive true
 end
 
+directory "/storage/nginx/html" do
+  owner 'nginx'
+  group 'nginx'
+  action :create
+  recursive true
+end
+
 template '/storage/nginx/nginx.conf' do
   source 'nginx.conf.erb'
+  variables ({ :confvars => {} })
+  owner 'nginx'
+  group 'nginx'
+  action :create_if_missing
+end
+
+template '/storage/nginx/conf.d/simple.conf' do
+  source 'site.conf.erb'
   variables ({ :confvars => {} })
   owner 'nginx'
   group 'nginx'
